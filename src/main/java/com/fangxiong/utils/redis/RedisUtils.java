a340edc74c31a5e58bdc051f51d7676afa391316
fangxiong <1255404327@qq.com>
2026-06-05 01:16:14 +0800

    Update functions of RedisUtils.

--- src/main/java/com/fangxiong/utils/redis/RedisUtils.java
+++ src/main/java/com/fangxiong/utils/redis/RedisUtils.java
@@ -204,22 +204,23 @@ public class RedisUtils {
 
     /**
      * 基于Redis自增的全局唯一Id生成器 格式：自定义时间戳位数，机器码位数，序列号位数(时间戳+机器码+序列号)
-     * @param keyPrefix Key值前缀 redis自增Key值格式：（icr：keyPrefix：startTime）
+     * @param keyPrefix Key值前缀 redis自增Key值格式：（keyPrefix：startTime:）
      * @param startTime 初始时间 时间戳格式：now - startTime
+     * @param timeKeyPattern 时间Key值格式
      * @param timeStampDigits Id中时间戳所占的位数
      * @param machineCodeDigits Id中机器码所占的位数
      * @param sequenceDigits Id中序列号所占的位数
      * @param machineCode 机器码
     */
-    public long uniqueIdGenerator(String keyPrefix,LocalDateTime startTime,int timeStampDigits,int machineCodeDigits,int sequenceDigits,long machineCode) {
+    public long uniqueIdGenerator(String keyPrefix,String timeKeyPattern,LocalDateTime startTime,int timeStampDigits,int machineCodeDigits,int sequenceDigits,long machineCode) {
         if(timeStampDigits+machineCodeDigits+sequenceDigits>MAX_DIGITS_OF_ID || timeStampDigits+machineCodeDigits+sequenceDigits<=0){
             throw new RuntimeException("设定的Id位数超出最大63位的限制！且最少为1位，The digits of the Id digits must be less than or equal to 63 and greater than or equal to 1");
         }
         long id = 0;long digitsCount = 0;
         LocalDateTime now = LocalDateTime.now();
-        String timeKey = now.format(DateTimeFormatter.ofPattern("yyyy:MM:dd"));
+        String timeKey = now.format(DateTimeFormatter.ofPattern(timeKeyPattern));
         long timeStampKey = now.toEpochSecond(ZoneOffset.UTC) - startTime.toEpochSecond(ZoneOffset.UTC);
-        Long sequenceNum = stringRedisTemplate.opsForValue().increment(INCREASE_PREFIX_KEY + keyPrefix + ":" + timeKey);
+        Long sequenceNum = stringRedisTemplate.opsForValue().increment(keyPrefix + ":" + timeKey);
         if(timeStampDigits!=0){
             id = timeStampKey<<(MAX_DIGITS_OF_ID-timeStampDigits);
             digitsCount+=timeStampDigits;
@@ -269,6 +270,14 @@ public class RedisUtils {
         LocalDateTime localDateTime = LocalDateTime.now();
         redisData.setExpireTime(calculateExpireTime(localDateTime,logicalTTL,timeUnit));
         stringRedisTemplate.opsForValue().set(fullKey,JsonUtils.beanToJson(redisData));
+    }
+
+    public Boolean enableLock(String key) {
+        return stringRedisTemplate.opsForValue().setIfAbsent(key, "1", 20, TimeUnit.SECONDS);
+    }
+
+    public Boolean disableLock(String key) {
+        return stringRedisTemplate.delete(key);
     }
 
     /**
